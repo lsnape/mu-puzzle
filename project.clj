56117ace970dbec8171d170b221e797bc3600a2f
Luke Snape <luke@likely.co>
2014-08-01 23:39:25 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,73 @@
+(defproject mu-puzzle ""
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.6.0"]
+
+                 [ring/ring-core "1.2.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+
+                 [prismatic/dommy "0.1.2"]
+
+                 [org.clojure/clojurescript "0.0-2234"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
+
+  :plugins [[jarohen/lein-frodo "0.3.2"]
+            [jarohen/simple-brepl "0.1.1"]
+            [lein-cljsbuild "1.0.3"]
+            [lein-pdo "0.1.1"]
+
+            [com.keminglabs/cljx "0.4.0"]
+            [lein-shell "0.4.0"]]
+
+  :frodo/config-resource "mu-puzzle-config.edn"
+
+  :source-paths ["src" "target/generated/clj"]
+
+  :resource-paths ["resources" "target/resources"]
+
+  :cljx {:builds [{:source-paths ["common-src"]
+                   :output-path "target/generated/clj"
+                   :rules :clj}
+
+                  {:source-paths ["common-src"]
+                   :output-path "target/generated/cljs"
+                   :rules :cljs}]}
+
+  :cljsbuild {:builds {:dev
+                       {:source-paths ["ui-src" "target/generated/cljs"]
+                        :compiler {:output-to "target/resources/js/mu-puzzle.js"
+                                   :output-dir "target/resources/js/"
+                                   :optimizations :whitespace
+                                   :pretty-print true
+
+                                   ;; uncomment for source-maps
+                                        ; :source-map "target/resources/js/mu-puzzle.js.map"
+                                   }}
+
+                       :prod
+                       {:source-paths ["ui-src" "target/generated/cljs"]
+                        :compiler {:output-to "target/resources/js/mu-puzzle.js"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :externs ["externs/jquery.js"]}}}}
+
+  :aliases {"dev" ["do"
+                   ["shell" "mkdir" "-p"
+                    "target/generated/clj"
+                    "target/generated/cljs"
+                    "target/resources"]
+                   ["cljx" "once"]
+                   ["pdo"
+                    ["cljx" "auto"]
+                    ["cljsbuild" "auto" "dev"]
+                    "frodo"]]
+            
+            "start" ["do"
+                     ["cljx" "once"]
+                     ["cljsbuild" "once" "prod"]
+                     ["trampoline" "frodo"]]})
