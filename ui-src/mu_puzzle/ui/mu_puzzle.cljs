1a8a270760c6cd2eb308d84175284cdda7bbace9
Luke Snape <luke@likely.co>
2014-08-07 22:07:15 +0100

    Moving to business logic to cljs

---
+++ ui-src/mu_puzzle/ui/mu_puzzle.cljs
@@ -0,0 +1,28 @@
+(ns mu-puzzle.ui.mu-puzzle
+  (:require [clojure.string :as s]))
+
+(defn i->iu [string]
+  (cond-> string
+    (re-matches #"M[UI]*I" string) (str "U")))
+
+(defn copy [string]
+  (str string (s/join (rest string))))
+
+(defn- replace-at [string idx ss ss-replace]
+  (let [[prefix suffix] ((juxt #(s/join (take idx %))
+                               #(s/join (drop idx %))) string)]
+
+    (str prefix (s/replace-first suffix (re-pattern ss) ss-replace))))
+
+(defn iii->u [string idx]
+  (replace-at string idx "III" "U"))
+
+(defn uuu-> [string idx]
+  (replace-at string idx "UU" ""))
+
+(defn indexes-of [s ss]
+  (->> s
+       (partition (count ss) 1)
+       (map-indexed vector)
+       (filter (comp #{(seq ss)} second))
+       (map first)))
