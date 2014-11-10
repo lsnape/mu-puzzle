284489f642f65c9538e20985aec7574085405c4d
Luke Snape <luke@likely.co>
2014-11-10 19:45:16 +0000

    Creating the mu component

--- src/cljs/mu_puzzle/core.cljs
+++ src/cljs/mu_puzzle/core.cljs
@@ -1,19 +1,32 @@
 (ns mu-puzzle.core
-  (:require [om.core :as om :include-macros true]
+  (:require [mu-puzzle.game :as game]
+            [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
-            [sablono.core :as html :refer-macros [html]]))
+            [sablono.core :as html :refer-macros [html]]
+            [cljs.core.async :as a]))
 
-(def app-state (atom {:text "MU Puzzle"}))
+(def app-state (atom {:text "MI"}))
+
+(defcomponent mu-text [text owner]
+  (render-state [_ _]
+    (html
+     [:p {:on-click (fn [e]
+                      (js/alert "woooo"))
+
+          :style {:text-align "center"
+                  :margin "8em auto"}}
+      text])))
 
 (defn main []
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (html
-           [:h1 {:style {:text-align "center", :margin "10em auto"}}
-            (:text app)]))))
+      (fn [{:keys [text] :as app} owner]
+        (reify
+          om/IRender
+          (render [_]
+            (html
+             [:h1 "MU Puzzle"
+              [:div
+               (om/build mu-text text)]]))))
     app-state
     {:target (. js/document (getElementById "app"))}))
