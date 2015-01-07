98e2664de4ac3f06817d9899e88d82a217a3e109
Luke Snape <luke@likely.co>
2015-01-07 13:50:07 +0000

    Formerly tests in cljx: need to port to cljs

--- test/mu_puzzle/game_test.clj
+++ test/mu_puzzle/game_test.clj
@@ -1,14 +1,18 @@
 (ns mu-puzzle.game-test
-  (:require [mu-puzzle.game :refer :all]
+  (:require ;; [mu-puzzle.ui.game :refer :all]
             [clojure.test :refer :all]))
 
-(deftest idx->group-test
-  (are [mu-string idx group] (= group (idx->group mu-string idx))
-       "MIII" 1 #{1 2 3}
-       "MIII" 2 #{1 2 3}
-       "MIII" 3 #{1 2 3}
-       "MIII" 4 nil
+;; TODO cljs testinate
 
-       "MIUIUIIIUIIUU" 11 #{11 12}
-       "MIUIUIIIUIIUU" 12 #{11 12}
-       "MIUIUIIIUIIUU" 13 nil))
+(comment
+  (deftest idx->group-test
+    (are [mu-string idx group] (= group (idx->group mu-string idx))
+         "MIII" 1 #{1 2 3}
+         "MIII" 2 #{1 2 3}
+         "MIII" 3 #{1 2 3}
+         "MIII" 4 nil
+
+         "MIUIUIIIUIIUU" 11 #{11 12}
+         "MIUIUIIIUIIUU" 12 #{11 12}
+         "MIUIUIIIUIIUU" 13 nil)))
+
