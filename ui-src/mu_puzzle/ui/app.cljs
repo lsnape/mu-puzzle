6b72105bc9c552dcfa27388b3095e8bd6b7b0258
Luke Snape <luke@likely.co>
2015-01-07 16:35:48 +0000

    core.async handlers in place for the buttons

--- ui-src/mu_puzzle/ui/app.cljs
+++ ui-src/mu_puzzle/ui/app.cljs
@@ -1,36 +1,46 @@
 (ns mu-puzzle.ui.app
   (:require [mu-puzzle.ui.game :as game]
             [clojure.string :as s]
+            [cljs.core.async :as a]
             [reagent.core :as reagent :refer [atom]]
             simple-brepl.client)
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (enable-console-print!)
 
-(defn render-miu-component [{:keys [miu-string]}]
+(defn render-miu-component [{:keys [miu-string]} game-ch]
   (for [ch miu-string]
     [:span.miu-ch ch]))
 
-(defn render-buttons []
+(defn render-buttons [game-ch]
   [:div.miu-buttons-outer
-   (for [[button-name handler] {"double" (fn []
-                                           (prn "TODO - double"))
+   (for [[action button-name :as button] {::double "double"
+                                          ::append-u "append U"
+                                          ::undo "undo"}]
+     
+     ^{:key action} [:div.miu-buttons
+                     [:button.btn.btn-default {:on-click (fn [e]
+                                                           (a/put! game-ch action)
+                                                           (.stopPropagation e))}
+                      button-name]])])
 
-                                "append U" (fn []
-                                             (prn "TODO - append U"))
-                               
-                                "undo" (fn []
-                                         (prn "TODO - undo"))}]
-     [:div.miu-buttons
-      [:button.btn.btn-default {:on-click handler}
-       button-name]])])
+(defn handle-game-events! [game-ch !miu-string]
+  (go-loop []
+    (condp = (a/<! game-ch)
+      ::double (prn "double")
+      ::append-u (prn "append u")
+      ::undo (prn "undo"))
+    
+    (recur)))
 
 (defn miu-component []
-  (let [!miu-state (atom {:miu-string "MIU"})]
+  (let [!miu-state (atom {:miu-string "MIU"})
+        game-ch (doto (a/chan)
+                  (handle-game-events! !miu-state))]
     (fn []
       [:div.miu-component
-       (render-miu-component @!miu-state)
-       (render-buttons)])))
+       (render-miu-component @!miu-state game-ch)
+       (render-buttons game-ch)])))
 
 (set! (.-onload js/window)
       (fn []
