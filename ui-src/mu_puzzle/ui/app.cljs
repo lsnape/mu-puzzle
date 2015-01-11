0eb8fcc28e488fc41a986c8838791fc62b3b27fc
Luke Snape <luke@likely.co>
2015-01-11 15:21:17 +0000

    App now working as before

--- ui-src/mu_puzzle/ui/app.cljs
+++ ui-src/mu_puzzle/ui/app.cljs
@@ -11,34 +11,52 @@
 
 (defn handle-char-events! [char-ch game-ch !miu-state]
   (go-loop []
-    (let [{:keys [action idx]} (a/<! char-ch)]
-      (cond
-        (= action ::char-over) (prn "over" idx)
-        (= action ::char-clicked) (prn "clicked" idx)))
-
+    (let [{:keys [action idx]} (a/<! char-ch)
+          {:keys [miu-string]} @!miu-state
+          highlights (game/idx->group miu-string idx)]
+      
+      (condp = action
+        ::char-over (swap! !miu-state assoc :highlights highlights)
+        
+        ::char-clicked (when-let [action (get {"III" ::iii->u "UU" ::uu->}
+                                              (game/idx->pattern miu-string idx))]
+                         
+                         (a/put! game-ch {:action action :idx idx}))
+        
+        ::mouse-out (swap! !miu-state assoc :highlights nil)))
+    
     (recur)))
 
 (defn render-miu-char [{:keys [miu-char idx]} !miu-state char-ch]
-  [:span.miu-ch {:on-mouse-over (fn [e]
-                                  (a/put! char-ch {:action ::char-over, :idx idx})
-                                  (.stopPropagation e))
+  (let [marked? (-> @!miu-state
+                    :highlights
+                    (contains? idx))]
+
+    [:span.miu-ch {:style {:background-color (when marked?
+                                               :blue)}
+
+                   :on-mouse-over (fn [e]
+                                    (a/put! char-ch {:action ::char-over, :idx idx})
+                                    (.stopPropagation e))
                  
-                 :on-click (fn [e]
-                             (a/put! char-ch {:action ::char-clicked, :idx idx})
-                             (.stopPropagation e))}
-   miu-char])
+                   :on-click (fn [e]
+                               (a/put! char-ch {:action ::char-clicked, :idx idx})
+                               (.stopPropagation e))}
+     miu-char]))
 
 (defn render-miu-component [!miu-state game-ch]
-  (let [!char-state (-> (:highlights @!miu-state)
-                        (wrap swap! !miu-state assoc :highlights))
+  (let [!char-highlights (-> (:highlights @!miu-state)
+                             (wrap swap! !miu-state assoc :highlights))
 
         char-event-ch (doto (a/chan)
-                        (handle-char-events! game-ch !char-state))]
+                        (handle-char-events! game-ch !miu-state))]
     (fn []
-      [:div
+      [:div {:on-mouse-out (fn [e]
+                             (a/put! char-event-ch {:action ::mouse-out})
+                             (.stopPropagation e))}
+       
        (for [[idx miu-char] (map-indexed vector (:miu-string @!miu-state))]
-         
-         ^{:key idx} [render-miu-char {:idx idx, :miu-char miu-char} !char-state char-event-ch])])))
+         ^{:key idx} [render-miu-char {:idx idx, :miu-char miu-char} !miu-state char-event-ch])])))
 
 (defn render-buttons [game-ch]
   [:div.miu-buttons-outer
@@ -52,31 +70,31 @@
                                                            (.stopPropagation e))}
                       button-name]])])
 
-(defn update-miu-string [action]
+(defn update-miu-string [action idx]
   (condp = action
     ::double game/copy
     ::append-u game/i->iu
-    ::iii->u identity
-    ::uu-> identity))
+    ::iii->u #(game/iii->u % idx)
+    ::uu-> #(game/uu-> % idx)))
 
 (defn handle-game-events! [game-ch !miu-state]
   (let [!undo-history (atom nil)
 
         undo-game-event! #(when-let [[previous-state] (seq @!undo-history)]
-                            (reset! !miu-state previous-state)
+                            (swap! !miu-state assoc :miu-string previous-state)
                             (swap! !undo-history rest))]
 
     (go-loop []
-      (let [{:keys [action] :as game-event} (a/<! game-ch)]
+      (let [{:keys [action idx] :as game-event} (a/<! game-ch)]
 
         (cond
           (= action ::undo) (undo-game-event!)
 
-          :game-events (let [current-state @!miu-state]
-                         (swap! !miu-state update :miu-string (update-miu-string action))
+          :game-events (let [current-miu-string (:miu-string @!miu-state)]
+                         (swap! !miu-state update :miu-string (update-miu-string action idx))
 
                          (when-not (= current-state @!miu-state)
-                           (swap! !undo-history (fnil conj (list)) current-state)))))
+                           (swap! !undo-history (fnil conj (list)) current-miu-string)))))
       (recur))))
 
 (defn miu-component []
